632f57a90601fe41b36abe055f8d77caeec4cee4
n-ric-v <n-ric-v@protonmail.com>
2019-01-05 20:07:45 +0100

    Correct misspelling device name

--- conf.js
+++ conf.js
@@ -1,4 +1,4 @@
-exports.deviceName 					= 'RDF Admin Bot';
+exports.deviceName 					= 'Admin RDF Multilingual Bot';
 exports.permanent_pairing_secret = '*';
 exports.hub 							= 'byteball.org/bb';
 exports.default_language 			= 'en';
